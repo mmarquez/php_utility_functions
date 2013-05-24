920486aea698c9192b87fc51a620dc8089530c88
Moisés Márquez <moises.marquez.g@gmail.com>
2013-05-24 11:34:24 +0300

    Update clean_characters.php

--- clean_characters.php
+++ clean_characters.php
@@ -3,7 +3,7 @@
  * Returns a string in uppercase, making the accents in their corresponding alternates and removing invalid characters.
  */
 function clean_characters($str){
-  $cadena = mb_convert_case($str, MB_CASE_UPPER, "UTF-8");
+  $str = mb_convert_case($str, MB_CASE_UPPER, "UTF-8");
 
   $pattern = array('À','Á','Â','Ã','Ä','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ñ','Ò','Ó','Ô','Õ','Ö','Ù','Ú','Û','Ü','Ý');
   $replacement = array('A','A','A','A','A','C','E','E','E','E','I','I','I','I','N','O','O','O','O','O','U','U','U','U','Y');
