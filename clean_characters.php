d5f0924a54eb71a5d5b416bc39d51ce6d4fad883
Moisés Márquez <moises.marquez.g@gmail.com>
2013-05-24 10:55:16 +0200

    Update clean_characters.php

--- clean_characters.php
+++ clean_characters.php
@@ -1,16 +1,18 @@
 <?php
 /**
  * Returns a string in uppercase, making the accents in their corresponding alternates and removing invalid characters.
+ * @param String $str To convert
+ * @param String $pattern To be used with the accepted characters
+ * @return String Converted string
  */
-function clean_characters($str){
-  $str = mb_convert_case($str, MB_CASE_UPPER, "UTF-8");
+function clean_characters($str, $pattern = "/[^0-9A-Z ]/"){
+  $str = mb_convert_case($cadena, MB_CASE_UPPER, "UTF-8");
 
-  $pattern = array('À','Á','Â','Ã','Ä','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ñ','Ò','Ó','Ô','Õ','Ö','Ù','Ú','Û','Ü','Ý');
-  $replacement = array('A','A','A','A','A','C','E','E','E','E','I','I','I','I','N','O','O','O','O','O','U','U','U','U','Y');
-  for ($i = 0; $i < sizeof($pattern); $i++) {
-    $str = mb_ereg_replace($pattern[$i], $replacement[$i], $str);
-  } 
+	$original = array('À','Á','Â','Ã','Ä','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ñ','Ò','Ó','Ô','Õ','Ö','Ù','Ú','Û','Ü','Ý');
+	$replacement = array('A','A','A','A','A','C','E','E','E','E','I','I','I','I','N','O','O','O','O','O','U','U','U','U','Y');
+	for ($i = 0; $i < sizeof($pattern); $i++) {
+	    $str = mb_ereg_replace($original[$i], $replacement[$i], $str);
+	} 
 
-  $pattern = "/[^0-9A-Z ]/";
-  return preg_replace($pattern, "", $str);
+	return preg_replace($pattern, "", $str);
 }
