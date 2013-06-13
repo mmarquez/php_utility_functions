dc57443407b2e727b276469f0ad4c7e7b897fa43
Moisés Márquez <moises.marquez.g@gmail.com>
2013-06-13 20:01:04 +0300

    Update clean_characters.php
    Added lower case, and Swedish, Norwegian and Rumanian characters.

--- clean_characters.php
+++ clean_characters.php
@@ -5,11 +5,11 @@
  * @param String $pattern To be used with the accepted characters
  * @return String Converted string
  */
-function clean_characters($str, $pattern = "/[^0-9A-Z ]/"){
+function clean_characters($str, $pattern = "/[^0-9A-Za-z ]/"){
   $str = mb_convert_case($cadena, MB_CASE_UPPER, "UTF-8");
 
-	$original = array('À','Á','Â','Ã','Ä','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ñ','Ò','Ó','Ô','Õ','Ö','Ù','Ú','Û','Ü','Ý');
-	$replacement = array('A','A','A','A','A','C','E','E','E','E','I','I','I','I','N','O','O','O','O','O','U','U','U','U','Y');
+	$original = array('Ă','Â','Å','À','Á','Â','Ã','Ä','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ñ','Ţ','Ò','Ó','Ô','Õ','Ö','Ù','Ú','Û','Ü','Ý','å','á','á','â','ã','ä','æ','ç','è','é','ê','ë','ì','í','î','ï','ñ','ò','ó','ô','õ','ö','ø','ù','ú','û','ü','ß','ş','ţ');
+	$replacement = array('A','A','A','A','A','A','A','A','C','E','E','E','E','I','I','I','I','N','T','O','O','O','O','O','U','U','U','U','Y','a','a','a','a','a','a','ae','c','e','e','e','e','i','i','i','i','n','o','o','o','o','o','o','u','u','u','u','ss','s','t');
 	for ($i = 0; $i < sizeof($pattern); $i++) {
 	    $str = mb_ereg_replace($original[$i], $replacement[$i], $str);
 	} 
